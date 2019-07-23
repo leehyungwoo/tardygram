5cb4d3b175f143ebf5a89f22169ceedf22a28559
leewoo <dlguddn3214@gmail.com>
2019-07-23 10:42:18 +0900

    renew

---
+++ frontend/src/views/examples/Login.js
@@ -0,0 +1,200 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React,{createRef} from "react";
+import axios from "axios"
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Row,
+  Col
+} from "reactstrap";
+
+class Login extends React.Component {
+
+  constructor(props){
+    super(props)
+     
+  }
+
+  state={
+    memberid:'',
+    pwd:''
+  }
+  email=createRef();
+
+  render() {
+    return (
+      <>
+        <Col lg="5" md="7">
+          <Card className="bg-secondary shadow border-0">
+            <CardHeader className="bg-transparent pb-5">
+              <div className="text-muted text-center mt-2 mb-3">
+                <small>Sign in with</small>
+              </div>
+              <div className="btn-wrapper text-center">
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <span className="btn-inner--icon">
+                    <img
+                      alt="..."
+                      src={require("assets/img/icons/common/github.svg")}
+                    />
+                  </span>
+                  <span className="btn-inner--text">Github</span>
+                </Button>
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <span className="btn-inner--icon">
+                    <img
+                      alt="..."
+                      src={require("assets/img/icons/common/google.svg")}
+                    />
+                  </span>
+                  <span className="btn-inner--text">Google</span>
+                </Button>
+              </div>
+            </CardHeader>
+            <CardBody className="px-lg-5 py-lg-5">
+              <div className="text-center text-muted mb-4">
+                <small>Or sign in with credentials</small>
+              </div>
+              <Form role="form">
+                <FormGroup className="mb-3">
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Email" type="email"  
+                      ref={ref => { this.email = ref }}
+                      onChange={(e)=>{
+                        this.setState({
+                          memberid:e.target.value
+                        })
+                      }}
+                    />
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input placeholder="Password" type="password"
+                      onChange={(e)=>{
+                        console.log(e.target.value)
+                        this.setState({
+                          pwd:e.target.value
+                        })
+                      }}
+                     />
+                  </InputGroup>
+                </FormGroup>
+                <div className="custom-control custom-control-alternative custom-checkbox">
+                  <input
+                    className="custom-control-input"
+                    id=" customCheckLogin"
+                    type="checkbox"
+                  />
+                  <label
+                    className="custom-control-label"
+                    htmlFor=" customCheckLogin"
+                  >
+                    <span className="text-muted">Remember me</span>
+                  </label>
+                </div>
+                <div className="text-center">
+                  <Button className="my-4" color="primary" type="button" onClick={(e)=>{
+                        console.log(this.state.memberid)
+                        let headers= {
+                          "Content-type":"application/json;charset=UTF-8"
+                        }
+                        axios.post(`http://localhost:9000/member/select`, 
+                          this.state,
+                           {headers: headers}) 
+                           .then(res=>{
+                              alert('성공')
+                            })
+                           .catch(e=>{
+                              alert(' 실패')
+                            })
+                     
+                        
+                        
+                        
+                           this.setState({
+                          memberid:'',
+                          pwd:''
+                        })
+
+                        this.props.history.push("/")
+                        
+                  }}>
+                    Sign in
+                  </Button>
+                </div>
+              </Form>
+            </CardBody>
+          </Card>
+          <Row className="mt-3">
+            <Col xs="6">
+              <a
+                className="text-light"
+                href="#pablo"
+                onClick={e => e.preventDefault()}
+              >
+                <small>Forgot password?</small>
+              </a>
+            </Col>
+            <Col className="text-right" xs="6">
+              <a
+                className="text-light"
+                href="#pablo"
+                onClick={e => e.preventDefault()}
+              >
+                <small>Create new account</small>
+              </a>
+            </Col>
+          </Row>
+        </Col>
+      </>
+    );
+  }
+}
+
+export default Login;
