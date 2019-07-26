7de8fedceeaaa54f1ff5f4b19d3b1cef9fefdcfb
landamaizer <landamaizer@gmail.com>
2019-07-26 11:11:26 +0900

    timepicker and password

--- frontend/src/views/examples/CreateHost.js
+++ frontend/src/views/examples/CreateHost.js
@@ -1,7 +1,8 @@
 import React, {Component, createRef} from "react";
 import axios from 'axios'
 import ReactDatetime from "react-datetime";
-// import TimePicker from './TimePicker.jsx'
+import 'react-times/css/material/default.css';
+import 'react-times/css/classic/default.css';
 import TimePickerWrapper from './TimePickerWrapper.js'
 import {
     Button,
@@ -72,8 +73,8 @@ class CreateHost extends Component {
             case 2:
                 return (
                     <div>
-                            <h1>set your group’s Time</h1>
-                            <h2>Set Group's timly room time. </h2>
+                            <h1>set your group’s Date and Time</h1>
+                            <h2>Set Group's timly room Date and time. </h2>
                             <FormGroup>
                             <InputGroup className="input-group-alternative">
                                 <InputGroupAddon addonType="prepend">
@@ -88,7 +89,7 @@ class CreateHost extends Component {
                                 timeFormat={false}
                                 />
                             </InputGroup>
-                            <TimePickerWrapper/>
+                            <TimePickerWrapper timeMode="12"/>
                             </FormGroup>
                     </div>
                 
@@ -99,20 +100,27 @@ class CreateHost extends Component {
                             <h1>Now describe what your group will be about</h1>
                             <h2>People will see this when we promote your group, but you’ll be able to add to it later, too</h2>
                     </div>
-                        );            
+                        );    
             case 4:
                 return (
                     <div>
-                            <h1>Upload group's profile picture</h1>
-                            <h2>Group's meet locally and in person. We’ll connect you with people who live in and around your area.</h2>
+                            <h1>set your group Password</h1>
                     </div>
-                        ); 
+                        );         
+            // case 4:
+            //     return (
+            //         <div>
+            //                 <h1>Upload group's profile picture</h1>
+            //                 <h2>Group's meet locally and in person. We’ll connect you with people who live in and around your area.</h2>
+            //         </div>
+            //             ); 
             case 5:
                 return (
                     <div>
                             <h1>Just set your group tardycash</h1>
                     </div>
                         ); 
+
             case 6:
                 return (
                     <div>
--- frontend/src/views/examples/TimePickerWrapper.js
+++ frontend/src/views/examples/TimePickerWrapper.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import TimePicker from './TimePicker.jsx';
+// import TimePicker from './TimePicker.jsx';
 import timeHelper from '../../utils/time';
 import ICONS from '../../utils/icons';
+import TimePicker from 'react-times';
+import 'react-times/css/material/default.css';
+import 'react-times/css/classic/default.css';
 
 class TimePickerWrapper extends React.Component {
   constructor(props) {
